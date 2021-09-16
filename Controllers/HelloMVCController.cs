3def9ec75279b706ff5a9fc91c17245d435e2ae5
mf721 <mf721@MATHEUSARAUJO-P>
2021-09-16 08:04:20 -0300

    Add project files.

---
+++ Controllers/HelloMVCController.cs
@@ -0,0 +1,76 @@
+﻿using HelloMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class HelloMVCController : Controller
+    {
+        // GET: HelloMVC
+        public ActionResult Index()
+        {
+            List<HelloMVCModel> model = new List<HelloMVCModel>();
+            
+            model.Add(new HelloMVCModel()
+            {
+                Nome = "Leozao",
+                Idade = 16,
+                BomAluno = true
+            });
+
+            model.Add(new HelloMVCModel()
+            {
+                Nome = "Parxceiro",
+                Idade = 17,
+                BomAluno = true
+            });
+
+            model.Add(new HelloMVCModel()
+            {
+                Nome = "Parxceiro",
+                Idade = 17,
+                BomAluno = true
+            });
+
+            model.Add(new HelloMVCModel()
+            {
+                Nome = "Parxceiro",
+                Idade = 17,
+                BomAluno = true
+            });
+
+            model.Add(new HelloMVCModel()
+            {
+                Nome = "Parxceiro",
+                Idade = 17,
+                BomAluno = true
+            });
+
+            model.Add(new HelloMVCModel()
+            {
+                Nome = "Parxceiro",
+                Idade = 17,
+                BomAluno = true
+            });
+
+            model.Add(new HelloMVCModel()
+            {
+                Nome = "Parxceiro",
+                Idade = 17,
+                BomAluno = true
+            });
+
+            model.Add(new HelloMVCModel()
+            {
+                Nome = "Parxceiro",
+                Idade = 17,
+                BomAluno = true
+            });
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/HelloMVCModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloMVC.Models
+{
+    public class HelloMVCModel
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public bool BomAluno { get; set; }
+    }
+}
